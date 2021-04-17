8de1a08652b2301aab7f225be4cf08baad963b9a
Apoorva Sambhav <apoorva4sambhav@gmail.com>
2021-04-17 16:19:03 -0400

    Updated DB

--- Project2.sql
+++ Project2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Apr 17, 2021 at 12:08 AM
+-- Generation Time: Apr 17, 2021 at 10:17 PM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 8.0.2
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `Project2`
+-- Database: `project2`
 --
 
 -- --------------------------------------------------------
@@ -47,7 +47,10 @@ INSERT INTO `employees` (`emp_id`, `emp_fname`, `emp_lname`, `emp_role`, `emp_ad
 (1, 'Apoorva', 'Sambhav', 'Admin', 'Brampton, Ontario', 'asambhav@myseneca.ca', '2019-04-01', 'Blue', 'Toronto'),
 (2, 'Ajay', 'Modgil', 'Admin', 'Brampton, Ontario', 'amodgil1@myseneca.ca', '2020-03-01', 'Red', 'Toronto'),
 (3, 'Jon', 'Snow', 'Janitor', '22 Bong', 'Snow.Jon@myseneca.ca', '2021-04-16', 'Orange', 'Hanoi'),
-(11, 'Matt', 'Modi', 'Janitor', 'New Palace, Toronto', 'Matt.Modi@myseneca.ca', '2021-04-16', 'Black', 'New Delhi');
+(19, 'Matt', 'Modi', 'Janitor', 'New Palace, Toronto', 'Matt.Modi@myseneca.ca', '2021-04-16', 'Black', 'New Delhi'),
+(20, 'Sal', 'All', 'Sales Rep', 'Not here', 'Sal.All@myseneca.ca', '2021-04-16', 'Pink', 'New York'),
+(21, 'Killa', 'Shahdad', 'Accountant', 'Ajax, Ontario', 'Killa.Shahdad@myseneca.ca', '2021-04-15', 'Black', 'Wuhan'),
+(22, 'Donkey', 'Kong', 'Sales Rep', 'Litty City', 'Donkey.kong@myseneca.ca', '2021-04-16', 'Red', 'Hamilton');
 
 -- --------------------------------------------------------
 
@@ -59,6 +62,14 @@ CREATE TABLE `login` (
   `emp_id` int(11) NOT NULL,
   `emp_passhash` varchar(64) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `login`
+--
+
+INSERT INTO `login` (`emp_id`, `emp_passhash`) VALUES
+(21, '2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824'),
+(22, 'd22f115abd6b44f9985037cfb39d6e1ab2db87a470d3e1081196562d420b8e41');
 
 --
 -- Indexes for dumped tables
@@ -74,7 +85,7 @@ ALTER TABLE `employees`
 -- Indexes for table `login`
 --
 ALTER TABLE `login`
-  ADD KEY `fk_import` (`emp_id`);
+  ADD PRIMARY KEY (`emp_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -84,7 +95,13 @@ ALTER TABLE `login`
 -- AUTO_INCREMENT for table `employees`
 --
 ALTER TABLE `employees`
-  MODIFY `emp_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `emp_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+
+--
+-- AUTO_INCREMENT for table `login`
+--
+ALTER TABLE `login`
+  MODIFY `emp_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- Constraints for dumped tables
